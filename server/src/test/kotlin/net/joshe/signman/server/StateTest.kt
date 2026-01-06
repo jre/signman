jre/signman

commit b3b3b52e1484c2b55472dd7066766ac93d33ecc6

Fix State.erase() to use default colors from the config.

server/src/main/kotlin/net/joshe/signman/server/State.kt
@@ -6,27 +6,27 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToStream
 import net.joshe.signman.api.IndexedColor
-import net.joshe.signman.api.RGB
 import net.joshe.signman.api.RGBColor
 import net.joshe.signman.api.SignColor
 import java.io.InputStream
 import java.io.OutputStream
 
 class State private constructor(
+    private val default: Snapshot,
     private var snap: Snapshot,
     private val onUpdate: (State.(Snapshot) -> Unit)? = null) {
     companion object {
-        private val default = Snapshot("",
-            fg = RGBColor(RGB(0, 0, 0)),
-            bg = RGBColor(RGB(255, 255, 255)))
-
-        fun initialize(text: String = "", fg: SignColor = default.fg, bg: SignColor = default.bg,
-                       onUpdate: State.(Snapshot) -> Unit)
-                = State(Snapshot(text, bg = bg, fg = fg), onUpdate = onUpdate)
+        fun initialize(config: Config.ColorConfig, text: String = "", fg: SignColor? = null, bg: SignColor? = null,
+                       onUpdate: State.(Snapshot) -> Unit) = State(
+            default = Snapshot("", fg = config.foreground, bg = config.background),
+            snap = Snapshot(text, bg = bg ?: config.foreground, fg = fg ?: config.background),
+            onUpdate = onUpdate)
 
         @OptIn(ExperimentalSerializationApi::class)
-        fun load(stream: InputStream, onUpdate: State.(Snapshot) -> Unit)
-                = State(Json.decodeFromStream<Snapshot>(stream), onUpdate)
+        fun load(config: Config.ColorConfig, stream: InputStream, onUpdate: State.(Snapshot) -> Unit) = State(
+            default = Snapshot("", fg = config.foreground, bg = config.background),
+            snap = Json.decodeFromStream<Snapshot>(stream),
+            onUpdate = onUpdate)
     }
 
     @OptIn(ExperimentalSerializationApi::class)

server/src/test/kotlin/net/joshe/signman/server/StateTest.kt
@@ -14,17 +14,20 @@ internal class StateTest {
     private val bg = RGBColor(RGB(0, 0, 255))
     private val otherFg = RGBColor(RGB(16, 16, 16))
     private val otherBg = RGBColor(RGB(0, 255, 0))
+    private val defFg = RGBColor(RGB(1, 1, 1))
+    private val defBg = RGBColor(RGB(254, 254, 254))
+    private val config: Config.ColorConfig = Config.RGBColorConfig(foreground = defFg, background = defBg)
 
     private val jsonText = """{"text":"TEST",""" +
             """"bg":{"type":"rgb","rgb":"${bg.rgb.toHexString()}"},""" +
             """"fg":{"type":"rgb","rgb":"${fg.rgb.toHexString()}"}}"""
 
     private fun load(f: State.(State.Snapshot) -> Unit = {}) = runBlocking {
-        State.load(ByteArrayInputStream(jsonText.toByteArray()), f)
+        State.load(config, ByteArrayInputStream(jsonText.toByteArray()), f)
     }
 
     private fun mk(f: (State.(State.Snapshot) -> Unit) = {}) = runBlocking {
-        State.initialize(text = "TEST", fg = fg, bg = bg, onUpdate = f)
+        State.initialize(config, text = "TEST", fg = fg, bg = bg, onUpdate = f)
     }
 
     @Test fun testStateLoad() = assertEquals(mk().snapshot, load().snapshot)
@@ -56,8 +59,6 @@ internal class StateTest {
     }
 
     @Test fun testStateErase(): Unit = runBlocking {
-        val black = RGBColor(RGB(0, 0, 0))
-        val white = RGBColor(RGB(255, 255, 255))
         var updateCount = 0
         var lastSnap: State.Snapshot
         val actual = load {
@@ -71,7 +72,7 @@ internal class StateTest {
         val res = actual.erase()
         assertEquals(1, updateCount)
         assertNotEquals(firstUpdate, lastSnap)
-        assertEquals(State.Snapshot("", fg = black, bg = white), res)
+        assertEquals(State.Snapshot("", fg = defFg, bg = defBg), res)
         assertEquals(res, lastSnap)
         assertEquals(res, actual.snapshot)
     }

server/src/main/kotlin/net/joshe/signman/server/main.kt
@@ -79,10 +79,10 @@ class SignmanServer : SuspendingCliktCommand() {
         }
         val state = try {
             file.inputStream().use { input ->
-                State.load(input, fn)
+                State.load(config.sign.color, input, fn)
             }
         } catch (_: Exception) {
-            State.initialize(onUpdate = fn)
+            State.initialize(config.sign.color, onUpdate = fn)
         }
         updated = MutableStateFlow(Cacheable.create(config, state.snapshot, renderer))
 

server/src/test/kotlin/net/joshe/signman/server/CacheableTest.kt
@@ -27,7 +27,7 @@ class CacheableTest {
     private val renderer = Renderer(conf, null)
 
     private fun defState(onUpdate: State.(State.Snapshot) -> Unit)
-            = State.initialize(defText, fg = defFg, bg = defBg, onUpdate)
+            = State.initialize(conf.sign.color, defText, fg = defFg, bg = defBg, onUpdate)
 
     @Test fun testDifferent() = runBlocking {
         var lastSnap: State.Snapshot

server/src/test/kotlin/net/joshe/signman/server/ServerTest.kt
@@ -127,11 +127,11 @@ class ServerTest {
 
     private fun Instantx.toHttpTime() = toLocalDateTime(TimeZone.UTC).format(httpTimeFmt)
 
-    private suspend fun mkStateRgb(onUpdate: State.(State.Snapshot) -> Unit)
-            = State.initialize(fg = defFgRgb, bg = defBgRgb, onUpdate = onUpdate)
+    private suspend fun mkStateRgb(config: Config, onUpdate: State.(State.Snapshot) -> Unit)
+            = State.initialize(config.sign.color, fg = defFgRgb, bg = defBgRgb, onUpdate = onUpdate)
 
-    private suspend fun mkStateIdx(onUpdate: State.(State.Snapshot) -> Unit)
-            = State.initialize(fg = defFgIdx, bg = defBgIdx, onUpdate = onUpdate)
+    private suspend fun mkStateIdx(config: Config, onUpdate: State.(State.Snapshot) -> Unit)
+            = State.initialize(config.sign.color, fg = defFgIdx, bg = defBgIdx, onUpdate = onUpdate)
 
     private fun IndexedColor.toRgb() = RGBColor(rgb)
 
@@ -176,13 +176,13 @@ class ServerTest {
     @Test fun testMainRgb() = mainTests(configRgb, uuidRgb, "alice", ::mkStateRgb)
     @Test fun testMainIndexed() = mainTests(configIdx, uuidIdx, "bob", ::mkStateIdx)
 
-    private fun mainTests(config: Config, uuid: Uuid, user: String, mkState: suspend (State.(State.Snapshot) -> Unit) -> State)
-    = runTest {
+    private fun mainTests(config: Config, uuid: Uuid, user: String,
+                          mkState: suspend (Config, State.(State.Snapshot) -> Unit) -> State) = runTest {
         val scope = this
         val renderer = Renderer(config, null)
         var updated = 0
         var flow: MutableStateFlow<Cacheable>? = null
-        val state = mkState { snap ->
+        val state = mkState(config) { snap ->
             updated++
             scope.launch { flow!!.emit(Cacheable.create(config, snap, renderer)) }
         }
@@ -308,12 +308,12 @@ class ServerTest {
     @Test fun testCacheRgb() = cacheTests(configRgb, uuidRgb, ::mkStateRgb)
     @Test fun testCacheIndexed() = cacheTests(configIdx, uuidIdx, ::mkStateIdx)
 
-    private fun cacheTests(config: Config, uuid: Uuid, mkState: suspend (State.(State.Snapshot) -> Unit) -> State)
-    = runTest {
+    private fun cacheTests(config: Config, uuid: Uuid,
+                           mkState: suspend (Config, State.(State.Snapshot) -> Unit) -> State) = runTest {
         val scope = this
         val renderer = Renderer(config, null)
         var flow: MutableStateFlow<Cacheable>? = null
-        val state = mkState { snap ->
+        val state = mkState(config) { snap ->
             scope.launch { flow!!.emit(Cacheable.create(config, snap, renderer)) }
         }
         flow = MutableStateFlow(Cacheable.create(config, state.snapshot, renderer))
@@ -411,11 +411,11 @@ class ServerTest {
     @Test fun testSseIndexed() = sseTests(configIdx, uuidIdx, "bob", ::mkStateIdx)
 
     private fun sseTests(config: Config, uuid: Uuid, user: String,
-                         mkState: suspend (State.(State.Snapshot) -> Unit) -> State) = runTest {
+                         mkState: suspend (Config, State.(State.Snapshot) -> Unit) -> State) = runTest {
         val scope = this
         val renderer = Renderer(config, null)
         var flow: MutableStateFlow<Cacheable>? = null
-        val state = mkState { snap ->
+        val state = mkState(config) { snap ->
             scope.launch { //(start = CoroutineStart.UNDISPATCHED) {
                 flow!!.emit(Cacheable.create(config, snap, renderer))
             }

commit 18e8eb4c5a27d282c42cc66017381e3513f9ee25

Fix initial State to use correct foreground and background color.
When a new State was being created after failing to load one from a
file, the foreground and background colors for the initial state were
swapped.

This was broken in b3b3b52e1484c2b55472dd7066766ac93d33ecc6 when State
was changed to use the default foreground and background color from
the Config.

This fixes the problem and adds a test which would have caught it.

server/src/main/kotlin/net/joshe/signman/server/State.kt
@@ -19,7 +19,7 @@ class State private constructor(
         fun initialize(config: Config.ColorConfig, text: String = "", fg: SignColor? = null, bg: SignColor? = null,
                        onUpdate: State.(Snapshot) -> Unit) = State(
             default = Snapshot("", fg = config.foreground, bg = config.background),
-            snap = Snapshot(text, bg = bg ?: config.foreground, fg = fg ?: config.background),
+            snap = Snapshot(text, fg = fg ?: config.foreground, bg = bg ?: config.background),
             onUpdate = onUpdate)
 
         @OptIn(ExperimentalSerializationApi::class)

server/src/test/kotlin/net/joshe/signman/server/StateTest.kt
@@ -80,4 +80,18 @@ internal class StateTest {
     @Test fun testSnapshotETag() {
         assertEquals("2o2j731", State.Snapshot("test", bg, fg).eTag())
     }
+
+    @Test fun testInitFgBgExplicit(): Unit = runBlocking {
+        val actual = State.initialize(config, "Initial text", fg, bg) {}
+        assertEquals(State.Snapshot("Initial text", fg = fg, bg = bg), actual.snapshot)
+        actual.erase()
+        assertEquals(State.Snapshot("", fg = defFg, bg = defBg), actual.snapshot)
+    }
+
+    @Test fun testInitFgBgDefault(): Unit = runBlocking {
+        val actual = State.initialize(config) {}
+        assertEquals(State.Snapshot("", fg = defFg, bg = defBg), actual.snapshot)
+        actual.erase()
+        assertEquals(State.Snapshot("", fg = defFg, bg = defBg), actual.snapshot)
+    }
 }